fb4ea40ae1e8bd6c9d6fd24a92927bdbf03161b0
silasms <silas.medeiros7@gmail.com>
2022-02-27 00:23:33 -0300

    final game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io; /* Biblioteca feita para entrada e saída(input/output) */
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1..101);
+
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Falied to read line");
+
+        let guess: u32 = match guess.trim().parse() {  // Permite esse match porque o parse retorna um Result(Ok ou Err)
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) { //Aqui nessa linha compara o guess com o secret_number
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                print!("You win!");
+                break;   
+            },
+        }
+    }
 }
